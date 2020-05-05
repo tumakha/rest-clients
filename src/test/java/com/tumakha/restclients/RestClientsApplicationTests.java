5736eda2526ccaf7a0ed3bc977efdfdaee848efd
Yuriy Tumakha <tumakha@gmail.com>
2020-05-05 18:13:03 +0100

    init Spring Boot

---
+++ src/test/java/com/tumakha/restclients/RestClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tumakha.restclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
